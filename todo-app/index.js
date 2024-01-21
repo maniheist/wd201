585410f98d52b551bce39e08bc8c99f707b2a077
maniheist <144818018+maniheist@users.noreply.github.com>
2024-01-21 17:33:55 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Started express server at port ${port}`);
+  console.log(`Started express server at port - ${port}`);
 });
