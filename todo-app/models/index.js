75209187bc5de5efc02cbbf62d6717abbbc8985e
maniheist <144818018+maniheist@users.noreply.github.com>
2024-01-22 15:18:04 +0530

    Update index.js

--- todo-app/models/index.js
+++ todo-app/models/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 "use strict";
 
 const fs = require("fs");
@@ -17,20 +18,23 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    config
+    config,
   );
 }
 
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
       sequelize,
-      Sequelize.DataTypes
+      Sequelize.DataTypes,
     );
     db[model.name] = model;
   });
