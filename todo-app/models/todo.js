4a5583613e49dccd0f8d26710564ef711cbfdb0d
maniheist <144818018+maniheist@users.noreply.github.com>
2024-01-22 11:08:36 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,30 +1,86 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
 
     static addTodo({ title, dueDate }) {
-      return this.create({ title: title, dueDate: dueDate, completed: false });
-    }
-
-    static getTodos() {
-      return this.findAll({
-        order: [["id", "ASC"]],
-      });
+      return this.create({ title, dueDate, completed: false });
     }
 
     markAsCompleted() {
       return this.update({ completed: true });
     }
+
+    deleteTodo() {
+      return this.removeTask(id); // Assuming removeTask is defined elsewhere
+    }
+
+    static getTodos() {
+      return this.findAll({ order: [["id", "ASC"]] });
+    }
+
+    static overdue() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: new Date().toLocaleDateString("en-CA"),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static dueToday() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date().toLocaleDateString("en-CA"),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static dueLater() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date().toLocaleDateString("en-CA"),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static completedItems() {
+      return this.findAll({
+        where: {
+          completed: true,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async remove(id) {
+      return this.destroy({
+        where: {
+          id,
+        },
+      });
+    }
+
+    setCompletionStatus(bool) {
+      return this.update({ completed: bool });
+    }
   }
+
   Todo.init(
     {
       title: DataTypes.STRING,
@@ -36,5 +92,6 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "Todo",
     }
   );
+
   return Todo;
 };
